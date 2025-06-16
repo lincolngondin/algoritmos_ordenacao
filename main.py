44ec14bd96325588aff4cd1bd9377b5b4176d4c9
jonas-ar <jonasfon@alu.ufc.br>
2025-06-15 21:55:42 -0300

    feat: adicionado quick-sort median-of-three

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ def test():
             return
 
 # test()
-        
+# quickSort utilizando pivô aleatório
 def partition(A, p, r):
     index_aleatorio = random.randint(p, r)
     A[index_aleatorio], A[r] = A[r], A[index_aleatorio]
@@ -66,3 +66,42 @@ def quickSort(A, p, r):
         quickSort(A, p, q - 1)
         quickSort(A, q + 1, r)
         # não há return. Faz a alteração no próprio vetor
+
+# quickSort utilizando median-of-three        
+def median_of_three(A, p, r):
+    meio = (p + r) // 2
+    
+    # encontra a mediana entre os três elementos
+    if A[p] <= A[meio] <= A[r] or A[r] <= A[meio] <= A[p]:
+        return meio
+    elif A[meio] <= A[p] <= A[r] or A[r] <= A[p] <= A[meio]:
+        return p
+    else:
+        return r
+    
+def partition_median(A, p, r):
+    # obtém o índice da mediana dos três
+    mediana_index = median_of_three(A, p, r)
+    # faz a troca da mediana com o último elemento
+    A[mediana_index], A[r] = A[r], A[mediana_index]
+    x = A[r] # o pivô agora é a mediana na posição r
+    i = p - 1
+    for j in range(p, r):
+        if A[j] <= x:
+            i = i + 1
+            A[j], A[i] = A[i], A[j]
+    A[r], A[i + 1] = A[i + 1], A[r]
+    return i + 1
+
+def quickSortMedian(A, p, r):
+    if p < r:
+        q = partition_median(A, p, r)
+        quickSortMedian(A, p, q - 1)
+        quickSortMedian(A, q + 1, r)
+        # não há return. Faz a alteração no próprio vetor
+        
+# Exemplo de uso:
+# V = [20, 30, 50, 1, 8, 5]
+# quickSort(V, 0, len(V) - 1)
+# W = [1, 5, 8, 5, 1, 2]
+# quickSortMedian(W, 0, len(W) - 1)
